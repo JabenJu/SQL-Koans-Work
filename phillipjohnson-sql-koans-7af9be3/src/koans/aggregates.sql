a0bf225c84c3f3b3885050c7a98ff1703c94c1db
JabenJu <89491072+JabenJu@users.noreply.github.com>
2022-02-01 10:04:29 -0500

    Update/Delete, Aggregates

--- phillipjohnson-sql-koans-7af9be3/src/koans/aggregates.sql
+++ phillipjohnson-sql-koans-7af9be3/src/koans/aggregates.sql
@@ -1,21 +1,21 @@
 -- Meditate on counting rows with COUNT
-select _____(*) from book
+select count(*) from book
 
 -- Meditate on counting within groups with GROUP BY
 select genre, count(*)
 from book
-_____ _____ genre
+group by genre
 
 -- Meditate on MAX
-select _____(datetime) from event
+select MAX(datetime) from event
 
 -- Meditate on MIN
-select _____(datetime) from event
+select MIN(datetime) from event
 
 -- Meditate on getting the maximum within groups
-select genre, _____(year_published)
+select genre, MAX(year_published)
 from book
-_____ _____ _____
+GROUP BY genre
 
 -- Meditate on joining data with aggregates
 select name, count(*)
